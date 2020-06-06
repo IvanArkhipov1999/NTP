889b1f89413faa21c55219ece87c28813847e574
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2020-06-06 12:25:51 +0300

    NTP

---
+++ ntp1.py
@@ -0,0 +1,28 @@
+from contextlib import closing
+from socket import socket, AF_INET, SOCK_DGRAM
+import struct
+import time
+import datetime
+
+time_format = "%a %b %d %H:%M:%S %Y"
+
+NTP_PACKET_FORMAT = "!12I"
+NTP_DELTA = 2208988800  # 1970-01-01 00:00:00
+NTP_QUERY = b'\x1b' + 47 * b'\0'
+
+
+def ntp_time(host="ru.pool.ntp.org", port=123):
+    with closing(socket(AF_INET, SOCK_DGRAM)) as s:
+        s.sendto(NTP_QUERY, (host, port))
+        msg, address = s.recvfrom(1024)
+    unpacked = struct.unpack(NTP_PACKET_FORMAT,
+                   msg[0:struct.calcsize(NTP_PACKET_FORMAT)])
+    return unpacked[10] + float(unpacked[11]) / 2**32 - NTP_DELTA
+
+
+if __name__ == "__main__":
+    print("Время с ntp сервера:")
+    print(time.ctime(ntp_time()))
+    print("Время на компьютере:")
+    a = datetime.datetime.now()
+    print(a.strftime(time_format))
